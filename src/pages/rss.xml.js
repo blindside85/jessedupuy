548b4f7512afcff7f10ba333e0a2280d9ef39be6
Jesse Dupuy <3085463+blindside85@users.noreply.github.com>
2024-07-06 13:22:05 -0700

    fix: debugging title tags part 3

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-    const currSite = context.site || 'https://jessedupuy.me';
+    const currSite = context.site;
     const posts = await getCollection("posts");
     return rss({
         title: 'jessedupuy: blog',
